90c55990e360dff29e7b95cc8ba378123a75a50b
Roberto Antonio Becerra García <idertator@gmail.com>
2023-10-29 20:27:08 +0100

    feat: se añaden ficheros de instalación del sistema

--- setup.py
+++ setup.py
@@ -30,5 +30,25 @@ setup(
     package_data={
         "bsp.adc": ["external/plux.so"],
     },
+    data_files=[
+        ("share/icons", ["data/BSPEog.png"]),
+        ("share/applications", ["data/BSPEog.desktop"]),
+    ],
     install_requires=requirements(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Typing :: Typed",
+    ],
+    python_requires=">=3.11",
+    include_package_data=True,
+    zip_safe=False,
 )
