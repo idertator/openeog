17960794a8f55a4623951313b4a7e7d478abd2b5
Roberto Antonio Becerra García <idertator@gmail.com>
2024-07-02 13:09:31 +0200

    fix: bug fixed

--- bsp/core/models/hardware.py
+++ bsp/core/models/hardware.py
@@ -7,7 +7,7 @@ class Hardware:
         acquisition_device: Device,
         acquisition_sampling_rate: int,
         stimuli_monitor: str,
-        stimuli_refresh_rate: int,
+        stimuli_monitor_refresh_rate: int,
         stimuli_monitor_width: int,
         stimuli_monitor_height: int,
         stimuli_monitor_resolution_width: int,
@@ -18,7 +18,7 @@ class Hardware:
         self.acquisition_sampling_rate = acquisition_sampling_rate
 
         self.stimuli_monitor = stimuli_monitor
-        self.stimuli_refresh_rate = stimuli_refresh_rate
+        self.stimuli_monitor_refresh_rate = stimuli_monitor_refresh_rate
         self.stimuli_monitor_width = stimuli_monitor_width
         self.stimuli_monitor_height = stimuli_monitor_height
         self.stimuli_monitor_resolution_width = stimuli_monitor_resolution_width
@@ -31,7 +31,7 @@ class Hardware:
             "acquisition_device": self.acquisition_device.value,
             "acqusition_sampling_rate": self.acquisition_sampling_rate,
             "stimuli_monitor": self.stimuli_monitor,
-            "stimuli_refresh_rate": self.stimuli_refresh_rate,
+            "stimuli_monitor_refresh_rate": self.stimuli_monitor_refresh_rate,
             "stimuli_monitor_width": self.stimuli_monitor_width,
             "stimuli_monitor_height": self.stimuli_monitor_height,
             "stimuli_monitor_resolution_width": self.stimuli_monitor_resolution_width,
