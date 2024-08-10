86fe9c2b734802c24697f65b1948c2e111d94c79
Sofia <soferblas@gmail.com>
2024-08-10 20:05:48 +0200

    Formato corregido

--- scripts/fixing_timing.py
+++ scripts/fixing_timing.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 
 BASE_PATH = "/Users/sofiafernandez/Desktop/TFG/Registros/july2024/"
 ANTISACCADIC_PATH = Path(BASE_PATH) / "antisaccades"
-# PURSUIT_PATH = Path(BASE_PATH) / "pursuits"
+PURSUIT_PATH = Path(BASE_PATH) / "pursuits"
 
 ANTISACCADIC_STUDIES = [
     "Prueba_Antisacadica_01.oeog",
@@ -40,37 +40,37 @@ ANTISACCADIC_STUDIES = [
     "Prueba_Antisacadica_35.oeog",
 ]
 
-# PURSUIT_STUDIES = [
-#     "Prueba_Persecucion_01.oeog",
-#     "Prueba_Persecucion_02.oeog",
-#     "Prueba_Persecucion_03.oeog",
-#     "Prueba_Persecucion_06.oeog",
-#     "Prueba_Persecucion_07.oeog",
-#     "Prueba_Persecucion_08.oeog",
-#     "Prueba_Persecucion_09.oeog",
-#     "Prueba_Persecucion_10.oeog",
-#     "Prueba_Persecucion_11.oeog",
-#     "Prueba_Persecucion_12.oeog",
-#     "Prueba_Persecucion_13.oeog",
-#     "Prueba_Persecucion_14.oeog",
-#     "Prueba_Persecucion_15.oeog",
-#     "Prueba_Persecucion_18.oeog",
-#     "Prueba_Persecucion_19.oeog",
-#     "Prueba_Persecucion_20.oeog",
-#     "Prueba_Persecucion_21.oeog",
-#     "Prueba_Persecucion_22.oeog",
-#     "Prueba_Persecucion_24.oeog",
-#     "Prueba_Persecucion_25.oeog",
-#     "Prueba_Persecucion_26.oeog",
-#     "Prueba_Persecucion_27.oeog",
-#     "Prueba_Persecucion_29.oeog",
-#     "Prueba_Persecucion_30.oeog",
-#     "Prueba_Persecucion_31.oeog",
-#     "Prueba_Persecucion_32.oeog",
-#     "Prueba_Persecucion_33.oeog",
-#     "Prueba_Persecucion_34.oeog",
-#     "Prueba_Persecucion_35.oeog",
-# ]
+PURSUIT_STUDIES = [
+    "Prueba_Persecucion_01.oeog",
+    "Prueba_Persecucion_02.oeog",
+    "Prueba_Persecucion_03.oeog",
+    "Prueba_Persecucion_06.oeog",
+    "Prueba_Persecucion_07.oeog",
+    "Prueba_Persecucion_08.oeog",
+    "Prueba_Persecucion_09.oeog",
+    "Prueba_Persecucion_10.oeog",
+    "Prueba_Persecucion_11.oeog",
+    "Prueba_Persecucion_12.oeog",
+    "Prueba_Persecucion_13.oeog",
+    "Prueba_Persecucion_14.oeog",
+    "Prueba_Persecucion_15.oeog",
+    "Prueba_Persecucion_18.oeog",
+    "Prueba_Persecucion_19.oeog",
+    "Prueba_Persecucion_20.oeog",
+    "Prueba_Persecucion_21.oeog",
+    "Prueba_Persecucion_22.oeog",
+    "Prueba_Persecucion_24.oeog",
+    "Prueba_Persecucion_25.oeog",
+    "Prueba_Persecucion_26.oeog",
+    "Prueba_Persecucion_27.oeog",
+    "Prueba_Persecucion_29.oeog",
+    "Prueba_Persecucion_30.oeog",
+    "Prueba_Persecucion_31.oeog",
+    "Prueba_Persecucion_32.oeog",
+    "Prueba_Persecucion_33.oeog",
+    "Prueba_Persecucion_34.oeog",
+    "Prueba_Persecucion_35.oeog",
+]
 
 OUTPUT_PATH = "processed_timing_fixed"
 
@@ -83,7 +83,6 @@ def process_study(study: Study):
         samples = len(test._hor_stimuli)
 
         # Fixing timing
-        SAMPLES_TO_CUT_FIRST_CALIBRATION = 222
         if  index == 0:
             hor_channel_cutted = test._hor_channel[222:]
             hor_stimuli_cutted = test._hor_stimuli[:-222]
@@ -104,10 +103,6 @@ def process_study(study: Study):
         test._ver_channel = ver_channel_processed
         test._ver_stimuli = ver_stimuli_cutted
 
-        print(f"Tamaño hor_channel: {len(test._hor_channel)}, Tamaño hor_stimuli: {len(test._hor_stimuli)}")
-        print(f"Tamaño ver_channel: {len(test._ver_channel)}, Tamaño ver_stimuli: {len(test._ver_stimuli)}")
-
-
 if __name__ == "__main__":
     ANTISACCADIC_OUTPUT_PATH = ANTISACCADIC_PATH / OUTPUT_PATH
     if not ANTISACCADIC_OUTPUT_PATH.exists():
@@ -125,18 +120,18 @@ if __name__ == "__main__":
             output_path = ANTISACCADIC_OUTPUT_PATH / filename
             save_study(study, output_path)
 
-    # PURSUIT_OUTPUT_PATH = PURSUIT_PATH / OUTPUT_PATH
-    # if not PURSUIT_OUTPUT_PATH.exists():
-    #     PURSUIT_OUTPUT_PATH.mkdir(parents=True)
+    PURSUIT_OUTPUT_PATH = PURSUIT_PATH / OUTPUT_PATH
+    if not PURSUIT_OUTPUT_PATH.exists():
+        PURSUIT_OUTPUT_PATH.mkdir(parents=True)
 
-    # for filename in tqdm(
-    #     PURSUIT_STUDIES,
-    #     desc="Processing pursuit studies",
-    # ):
-    #     fullpath = PURSUIT_PATH / filename
-    #     if fullpath.exists():
-    #         study = load_study(fullpath)
-    #         process_study(study)
+    for filename in tqdm(
+        PURSUIT_STUDIES,
+        desc="Processing pursuit studies",
+    ):
+        fullpath = PURSUIT_PATH / filename
+        if fullpath.exists():
+            study = load_study(fullpath)
+            process_study(study)
 
-    #         output_path = PURSUIT_OUTPUT_PATH / filename
-    #         save_study(study, output_path)
+            output_path = PURSUIT_OUTPUT_PATH / filename
+            save_study(study, output_path)
